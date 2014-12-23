0be7703c6a20bebc0c50d7a215d9fe4a7448f056
Ferry Wang <foomango@gmail.com>
2014-12-23 17:47:13 +0800

    int argument error

--- argparse/prog.py
+++ argparse/prog.py
@@ -20,6 +20,6 @@ __author__ = 'fewang@microstrategy.com (Fengwei Wang)'
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('echo', help='echo the string you use here')
+parser.add_argument('square', help='display a square of a given number')
 args = parser.parse_args()
-print args.echo
+print args._square ** 2
