70cc6deb9ea8242ca7220e55e2bfb4870b148794
Ferry Wang <foomango@gmail.com>
2014-12-23 18:42:09 +0800

    Finish demo

--- argparse/prog.py
+++ argparse/prog.py
@@ -19,8 +19,19 @@ __author__ = 'fewang@microstrategy.com (Fengwei Wang)'
 
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('square', help='display a square of a given number',
-                    type=int)
+parser = argparse.ArgumentParser(description='calculate X to the power of Y')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-v', '--verbose', action='store_true',
+                   help='increase output verbosity')
+group.add_argument('-q', '--quiet', action='store_true')
+parser.add_argument('x', type=int, help='the base')
+parser.add_argument('y', type=int, help='the exponent')
 args = parser.parse_args()
-print args.square ** 2
+answer = args.x ** args.y
+
+if args.quiet:
+    print answer
+elif args.verbose:
+    print '{} to the power {} equals {}'.format(args.x, args.y, answer)
+else:
+    print '{}^{} == {}'.format(args.x, args.y, answer)
